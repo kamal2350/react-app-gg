04dc1df7e7f6485c6c2e005cf95cb29c25b68660
kamal Kishore <kamal.kishore@vitas.com>
2023-06-27 15:54:15 -0400

    Update Api Route

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-// export const HostName="https://green-groccers.onrender.com"
-export const HostName="http://localhost:4000"
+export const HostName="https://green-groccers.onrender.com"
+// export const HostName="http://localhost:4000"
